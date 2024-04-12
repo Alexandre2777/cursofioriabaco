2a2bebb615e5c608c7078d83642dbb816533ae40
Eduardo A <eduardo.abreu@viaconsulting.com.br>
2024-04-11 22:20:48 -0300

    commit 4

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,6 +10,47 @@ sap.ui.define([
 		return Controller.extend("zcursoaaapp1.controller.View1", {
 			onInit: function () {
  
-			}
+			},
+
+
+			onPress: function () {
+				
+				var that = this ;
+
+				 var idusuario = this.byId("idusuario").getValue();
+				 var idnome = this.byId("idnome").getValue();
+				 var idemail = this.byId("idemail").getValue();
+
+				//if (!idusuario){
+					//sap.m.MessageToast("Campo Usuario Obrigatorio");
+					//sap.m.MessageBox.error("Campo Usuario Obrigatorio");
+					//return; 
+				//}
+
+				 var oDados = {
+					Usuario: idusuario,
+					Nome: idnome,
+					Email: idemail
+				 }
+				
+
+
+				this.getView().getModel().create("/Curso001Set", oDados, { 
+					success: function (oData, resposta) {
+
+						that.byId("idusuario").setValue("");
+						that.byId("idnome").setValue("");
+						that.byId("idemail").setValue("");
+
+						//sap.m.MessageBox.success("Sucesso");
+					  
+					},
+					error: function (e) {
+						sap.m.MessageBox.error("Erro");
+					}
+				  });
+				},
+
+
 		});
 	});
